8de3e1f34e0363747ed60913f29bd2dec4607afe
Vitor Albiero <vitor_albiero@outlook.com>
2019-06-21 11:06:32 -0400

    Fixed computed ranks when probe == gallery

--- get_cmc_ranks.py
+++ get_cmc_ranks.py
@@ -29,16 +29,26 @@ def get_ranks(authentic_file, impostor_file, label_file_probe, label_file_galler
         label_idx = label_probe[i, 0]
 
         rank = 0
-        best_authentic = np.max(authentic[authentic[:, 0] == label_idx][:, 2].astype(float))
+        label_scores = authentic[authentic[:, 0] == label_idx][:, 2].astype(float)
+
+        best_authentic = -float('Inf')
+        # check if subject with label_idx have scores in collum 1
+        if len(label_scores) > 0:
+            best_authentic = np.max(label_scores)
 
         if label_gallery is None:
-            best_authentic = max(best_authentic, np.max(authentic[authentic[:, 1] == label_idx][:, 2].astype(float)))
+            label_scores = authentic[authentic[:, 1] == label_idx][:, 2].astype(float)
+
+            # check if subject with label_idx have scores in collum 2 (only when probe == gallery)
+            if len(label_scores) > 0:
+                best_authentic = max(best_authentic, np.max(label_scores))
 
         impostor_scores = impostor[impostor[:, 0] == label_idx][:, 2].astype(float)
         rank = len(impostor_scores[impostor_scores > best_authentic])
 
         if label_gallery is None:
             impostor_scores = impostor[impostor[:, 1] == label_idx][:, 2].astype(float)
+
             rank += len(impostor_scores[impostor_scores > best_authentic])
 
         rank += 1
@@ -49,7 +59,7 @@ def get_ranks(authentic_file, impostor_file, label_file_probe, label_file_galler
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Plot Score Histogram')
+    parser = argparse.ArgumentParser(description='Compute Ranks')
     parser.add_argument('-authentic', '-a', help='Authentic 1 scores.')
     parser.add_argument('-impostor', '-i', help='Impostor 1 scores.')
     parser.add_argument('-label_probe', '-lp', help='Label probe.')
