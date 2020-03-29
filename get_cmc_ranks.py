50b0f488a6c8d356e1b8cf7df4ed7856e7cd4938
Vitor Albiero <vitor_albiero@outlook.com>
2020-03-28 20:29:05 -0400

    Fixed cmc ranks

--- get_cmc_ranks.py
+++ get_cmc_ranks.py
@@ -1,7 +1,6 @@
 import numpy as np
 import argparse
-import matplotlib.pyplot as plt
-from os import path, makedirs
+from os import path
 
 
 def load_files(authentic_file, impostor_file, label_file_probe, label_file_gallery):
@@ -17,44 +16,83 @@ def load_files(authentic_file, impostor_file, label_file_probe, label_file_galle
     return authentic, impostor, label_probe, label_gallery
 
 
-def get_ranks(authentic_file, impostor_file, label_file_probe, label_file_gallery):
+def get_ranks(authentic_file, impostor_file, label_file_probe, label_file_gallery, distance):
     ranks_save = label_file_probe[:-4] + '_ranks.txt'
+    best_auth_imp_save = label_file_probe[:-4] + '_label_auth_imp_diff.txt'
 
     authentic, impostor, label_probe, label_gallery = load_files(
         authentic_file, impostor_file, label_file_probe, label_file_gallery)
 
     ranks = []
+    best_auth_imp = []
 
     for i in range(len(label_probe)):
         label_idx = label_probe[i, 0]
 
         rank = 0
-        label_scores = authentic[authentic[:, 0] == label_idx][:, 2].astype(float)
+        authentic_scores = authentic[authentic[:, 0] == label_idx][:, 2].astype(float)
 
-        best_authentic = -float('Inf')
+        if distance:
+            best_authentic = float('Inf')
+            best_impostor = float('Inf')
+        else:
+            best_authentic = -float('Inf')
+            best_impostor = -float('Inf')
+
         # check if subject with label_idx have scores in collum 1
-        if len(label_scores) > 0:
-            best_authentic = np.max(label_scores)
+        if len(authentic_scores) > 0:
+            if distance:
+                best_authentic = np.min(authentic_scores)
+            else:
+                best_authentic = np.max(authentic_scores)
 
         if label_gallery is None:
-            label_scores = authentic[authentic[:, 1] == label_idx][:, 2].astype(float)
+            authentic_scores = authentic[authentic[:, 1] == label_idx][:, 2].astype(float)
 
             # check if subject with label_idx have scores in collum 2 (only when probe == gallery)
-            if len(label_scores) > 0:
-                best_authentic = max(best_authentic, np.max(label_scores))
+            if len(authentic_scores) > 0:
+                if distance:
+                    best_authentic = min(best_authentic, np.min(authentic_scores))
+                else:
+                    best_authentic = max(best_authentic, np.max(authentic_scores))
+
+        # check if label has an authentic score, otherwise skip
+        if best_authentic == -float('Inf') or best_authentic == float('Inf'):
+            continue
 
         impostor_scores = impostor[impostor[:, 0] == label_idx][:, 2].astype(float)
-        rank = len(impostor_scores[impostor_scores > best_authentic])
+
+        if distance:
+            rank = len(impostor_scores[impostor_scores < best_authentic])
+            best_impostor = np.min(impostor_scores)
+        else:
+            rank = len(impostor_scores[impostor_scores > best_authentic])
+            best_impostor = np.max(impostor_scores)
 
         if label_gallery is None:
             impostor_scores = impostor[impostor[:, 1] == label_idx][:, 2].astype(float)
 
-            rank += len(impostor_scores[impostor_scores > best_authentic])
+            if distance:
+                rank += len(impostor_scores[impostor_scores < best_authentic])
+                best_impostor = min(best_impostor, np.min(impostor_scores))
+            else:
+                rank += len(impostor_scores[impostor_scores > best_authentic])
+                best_impostor = max(best_impostor, np.max(impostor_scores))
+
+        # check if label has an impostor score, otherwise skip
+        if best_impostor == -float('Inf') or best_impostor == float('Inf'):
+            continue
 
         rank += 1
 
         ranks.append(rank)
+        best_auth_imp.append([label_idx, best_authentic, best_impostor,
+                              round(best_authentic - best_impostor, 6)])
 
+    best_auth_imp = np.array(best_auth_imp)
+    best_auth_imp = best_auth_imp[best_auth_imp[:, 3].argsort()]
+
+    np.savetxt(best_auth_imp_save, best_auth_imp, delimiter=' ', fmt='%s')
     np.savetxt(ranks_save, ranks, delimiter=' ', fmt='%s')
 
 
@@ -64,7 +102,9 @@ if __name__ == '__main__':
     parser.add_argument('-impostor', '-i', help='Impostor 1 scores.')
     parser.add_argument('-label_probe', '-lp', help='Label probe.')
     parser.add_argument('-label_gallery', '-lg', help='Label gallery.')
+    parser.add_argument('--distance', '-d', help='Distance or similarity metric.', action='store_true')
 
     args = parser.parse_args()
+    print(args.distance)
 
-    get_ranks(args.authentic, args.impostor, args.label_probe, args.label_gallery)
+    get_ranks(args.authentic, args.impostor, args.label_probe, args.label_gallery, args.distance)
